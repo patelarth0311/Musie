1563d863d49d4a7d33b785b6632eaca8f0a28f55
patelarth0311 <patelarth0311@gmail.com>
2023-08-10 00:05:22 -0400

    Initial Commit

---
+++ Musie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Musie
+//
+//  Created by Arth Patel on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Musie/MusieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusieApp.swift
+//  Musie
+//
+//  Created by Arth Patel on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MusieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
